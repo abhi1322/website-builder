d948a04f84b21be5004357d8fe10d37c3e364b77
abhi1322 <abhi1322jii@gmail.com>
2024-07-06 13:18:04 +0530

    Navigation ui setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { ThemeProvider } from "@/providers/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -21,8 +22,15 @@ export default function RootLayout({
         baseTheme: dark,
       }}
     >
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <body className={inter.className}>{children}</body>
+        </ThemeProvider>
       </html>
     </ClerkProvider>
   );
--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -3,13 +3,26 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="h-full w-full pt-36 relative flex items-center justify-center flex-col">
-
       <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
       <p className="text-center">Run your agency, in one place</p>
       <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
-        <h1 className="text-9xl font-bold text-center md:text-[300px]">Plura</h1>
+        <h1 className="text-9xl font-bold text-center md:text-[300px]">
+          Plura
+        </h1>
       </div>
-      <div className="flex items-center justify-center relative md:mt-[-70px"></div>
+      <div className="flex items-center justify-center relative md:mt-[-70px">
+        <Image
+          src={"/assets/preview.png"}
+          alt="banner image"
+          width={1200}
+          height={1200}
+          className="rounded-tl-xl rounded-tr-2xl border-2 border-muted"
+        />
+        <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute"></div>
+      </div>
+      <section className="flex justify-center flex-col gap-4 mt-20">
+        <h2 className="text-4xl text-center">Choose what fits right</h2>
+      </section>
     </main>
   );
 }
